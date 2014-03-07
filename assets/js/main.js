7d8460deaca54fc435099b22fb8739886e4856b4
Jake Larson <jake.j.larson@gmail.com>
2014-03-06 23:04:35 -0600

    add no-bg class to mobile menu when nav is open

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,12 +20,14 @@ $(document).ready(function() {
 
   $('.btn-mobile-menu').click(function() {
     $('.navigation-wrapper').toggleClass('visible animated bounceInDown');
-    $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn no-bg');
+    $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn');
+    $(this).toggleClass('no-bg');
   });
 
   $('.navigation-wrapper .blog-button').click(function() {
     $('.navigation-wrapper').toggleClass('visible');
-    $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn no-bg');
+    $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn');
+    $('.btn-mobile-menu').toggleClass('no-bg');
   });
 
 });
